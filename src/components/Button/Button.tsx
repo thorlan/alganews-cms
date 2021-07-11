8aa0e27ef51e2b958ac9725348bcaf5709fc8156
Thiago Orlandini <thiagorlandini@hotmail.com>
2021-07-11 10:55:13 -0300

    first

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,14 @@
+import * as B from './Button.styles';
+
+export interface ButtonProps {
+
+}
+
+export default function Button({ }: ButtonProps) {
+    return (
+        <B.Wrapper>
+            Botão
+        </B.Wrapper>
+    )
+
+}
