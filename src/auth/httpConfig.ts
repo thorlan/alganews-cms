0170fd8df522d43d8e8cc95c2d350d73cade3685
thorlan <thiagorlandini@hotmail.com>
2022-04-16 14:01:27 -0300

    ajustes no httpconfig para fila de refresh token

--- src/auth/httpConfig.ts
+++ src/auth/httpConfig.ts
@@ -1,22 +1,37 @@
-import axios from 'axios';
-import Service from 'danielbonifacio-sdk/dist/Service';
-import AuthService from './Authorization.service';
+import axios from "axios";
+import Service from "danielbonifacio-sdk/dist/Service";
+import AuthService from "./Authorization.service";
 
-const { REACT_APP_API_URL } = process.env;
-if (REACT_APP_API_URL) {
-    Service.setBaseUrl(REACT_APP_API_URL);
-}
+const { REACT_APP_API_BASE_URL } = process.env;
+
+if (REACT_APP_API_BASE_URL) Service.setBaseUrl(REACT_APP_API_BASE_URL);
 
 Service.setRequestInterceptors(async (request) => {
     const accessToken = AuthService.getAccessToken();
 
     // injeta o token de acesso na requisição
     if (accessToken) {
-        request.headers['Authorization'] = `Bearer ${accessToken}`;
+        request.headers["Authorization"] = `Bearer ${accessToken}`;
     }
 
     return request;
 });
+
+// for multiple requests
+let isRefreshing: boolean = false;
+let failedQueue: any[] = [];
+
+const processQueue = (error: any, token: string | null = null) => {
+    failedQueue.forEach((prom) => {
+        if (error) {
+            prom.reject(error);
+        } else {
+            prom.resolve(token);
+        }
+    });
+
+    failedQueue = [];
+};
 
 Service.setResponseInterceptors(
     (response) => response,
@@ -26,7 +41,21 @@ Service.setResponseInterceptors(
 
         // caso o erro seja de autenticação e ainda não foi feito o retry
         if (error?.response?.status === 401 && !originalRequest._retry) {
+            if (isRefreshing) {
+                return new Promise(function (resolve, reject) {
+                    failedQueue.push({ resolve, reject });
+                })
+                    .then((token) => {
+                        originalRequest.headers["Authorization"] = "Bearer " + token;
+                        return axios(originalRequest);
+                    })
+                    .catch((err) => {
+                        return Promise.reject(err);
+                    });
+            }
+
             originalRequest._retry = true;
+            isRefreshing = true;
 
             // recupera o code verifier e o refresh token
             const storage = {
@@ -42,23 +71,32 @@ Service.setResponseInterceptors(
                 return;
             }
 
-            // renova o token
-            const tokens = await AuthService.getNewToken({
-                codeVerifier,
-                refreshToken,
-            });
+            try {
+                // renova o token
+                const tokens = await AuthService.getNewToken({
+                    codeVerifier,
+                    refreshToken,
+                });
 
-            // armazena os tokens para novas requisições
-            AuthService.setAccessToken(tokens.access_token);
-            AuthService.setRefreshToken(tokens.refresh_token);
+                // armazena os tokens para novas requisições
+                AuthService.setAccessToken(tokens.access_token);
+                AuthService.setRefreshToken(tokens.refresh_token);
 
-            // implementa o token na requisição
-            originalRequest.headers[
-                'Authorization'
-            ] = `Bearer ${tokens.access_token}`;
+                // implementa o token na requisição
+                originalRequest.headers[
+                    "Authorization"
+                ] = `Bearer ${tokens.access_token}`;
 
-            // retorna uma nova chamada do axios com essa requisição
-            return axios(originalRequest);
+                processQueue(null, tokens.access_token);
+
+                // retorna uma nova chamada do axios com essa requisição
+                return axios(originalRequest);
+            } catch (err) {
+                processQueue(err, null);
+                throw err;
+            } finally {
+                isRefreshing = false;
+            }
         }
 
         throw error;
